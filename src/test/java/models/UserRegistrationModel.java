937664166799bafcb5dd94a416a989d1e3330278
Jul112 <julia.bv@gmail.com>
2021-06-17 01:21:20 +0300

    Models

---
+++ src/test/java/models/UserRegistrationModel.java
@@ -0,0 +1,13 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class UserRegistrationModel {
+    private String email;
+    private String password;
+}
